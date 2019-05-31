df88abdd1322446ea5d658ab321ea02ee5829228
Kelvin Homann <kelvin.homann@gmail.com>
2019-05-31 10:34:46 +0200

    shader fix;

--- Planetsystem/example_glfw_opengl3/sphere.v.glsl
+++ Planetsystem/example_glfw_opengl3/sphere.v.glsl
@@ -1,18 +1,9 @@
-#version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aNormal;
-
-out vec3 FragPos;
-out vec3 Normal;
+attribute vec3 coord3d;
+attribute vec3 v_color;
 
 uniform mat4 mvp;
-uniform mat4 view;
-uniform mat4 projection;
 
 void main()
 {
-    FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = mat3(transpose(inverse(model))) * aNormal;  
-    
-    gl_Position = projection * view * vec4(FragPos, 1.0);
+    gl_Position = mvp * vec4(coord3d, 1.0);
 }
